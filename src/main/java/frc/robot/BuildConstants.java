7362affc24c0dd9c6a3470a8412652a4afe05383
Anshu <acharya.anshu@gmail.com>
2025-05-11 11:35:15 -0700

    build constants updated for some reason

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "DifferentialSwerve";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 7;
-  public static final String GIT_SHA = "5844e91b69efbfc66717e60a995766ca638f74b9";
-  public static final String GIT_DATE = "2025-05-07 21:33:24 CDT";
+  public static final int GIT_REVISION = 8;
+  public static final String GIT_SHA = "df77965ca7d1480fdfa352ab51eaf723c14af3d8";
+  public static final String GIT_DATE = "2025-05-07 21:40:52 CDT";
   public static final String GIT_BRANCH = "oneModule";
-  public static final String BUILD_DATE = "2025-05-07 21:33:36 CDT";
-  public static final long BUILD_UNIX_TIME = 1746671616342L;
+  public static final String BUILD_DATE = "2025-05-11 13:12:12 CDT";
+  public static final long BUILD_UNIX_TIME = 1746987132368L;
   public static final int DIRTY = 0;
 
   private BuildConstants(){}
